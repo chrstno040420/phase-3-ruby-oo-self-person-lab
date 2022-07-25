9c4557dcf483ae0ce1418c8089b1fa5773171d23
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-07-25 17:02:23 -0500

    commit tree c4a59e61c7e13344461fbe0dcb6692d2e1f09980

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+require "pry"
+
+class Person
+
+attr_reader :name
+attr_accessor :bank_account, :happiness, :hygiene
+
+@@all = []
+
+def initialize(name)
+    @name = name
+end
+
+# def initialize(attributes = {})
+# attributes.each do |attribute , value| 
+#   self.send("#{attribute}=" , value)
+# end
+# @@all << self
+# end
+
+
+binding.pry 
+end
+
